2ca77f4068e50bfbc24ff8e11c418b4dc9bfb407
cindy <14489918@qq.com>
2018-07-24 11:39:33 +0800

    update wc

--- src/main/scala/com/scala/D4/WordCountApp.scala
+++ src/main/scala/com/scala/D4/WordCountApp.scala
@@ -10,23 +10,36 @@ object WordCountApp {
 
     val lines = Array("hello hadoop scala hdfs", "hello scala sqoop hadoop", "hello hdfs hive", "hello scala")
 
-    // Map then flatten
-    val words = lines.flatMap(_.split(" "))
+//    // Map then flatten
+//    val words = lines.flatMap(_.split(" "))
+//
+//    // word => (word, 1)
+//    val wordToTuple = words.map((_,1))
+//
+//    // groupby key
+//    val grouped = wordToTuple.groupBy(_._1)
+//
+//    // tuple(key, length)
+//    val wordList = grouped.map(t => (t._1,t._2.length))
+//
+//    // sort by value(count)
+//    println(wordList.toList.sortBy(_._2))
+//
+//    // reverse
+//    println(wordList.toList.sortBy(_._2).reverse)
 
-    // word => (word, 1)
-    val wordToTuple = words.map((_,1))
+    val words = lines
+      .flatMap(_.split(" "))      // map then fatten
+      .map((_,1))                         // word => (word,1)
+      .groupBy(_._1)                      // group by key
+      .map(t => (t._1,t._2.length))       // new tuple(key,length)
+      .toList                             // tuple to List
+      .sortBy(_._2)                       // sort by value
+      .reverse                            // reverse
 
-    // groupby key
-    val grouped = wordToTuple.groupBy(_._1)
-
-    // tuple(key, length)
-    val wordList = grouped.map(t => (t._1,t._2.length))
-
-    // sort by value(count)
-    println(wordList.toList.sortBy(_._2))
-
-    // reverse
-    println(wordList.toList.sortBy(_._2).reverse)
+    for (ele <- words) {
+      println(ele)
+    }
   }
 
 }
